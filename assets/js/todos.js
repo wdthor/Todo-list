bdc8fc43d12565ee4f8d70123fb99920fdc1adb0
webdevthierry <thierry.hor.sd@gmail.com>
2017-08-29 01:49:18 +0200

    Design and animations added

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -17,6 +17,11 @@ $("input[type='text']").keypress(function(event) {
 		var todoText = $(this).val();
 		$(this).val("");
 		// create a new li and add to ul
-		$("ul").append("<li><span>X</span> " +todoText+ "</li>");
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " +todoText+ "</li>");
 	}
+});
+
+$(".fa-minus").on("click", function() {
+	$("input[type='text']").fadeToggle();
+	$(this).toggleClass("fa-minus fa-plus");
 })
